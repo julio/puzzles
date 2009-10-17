98294759a9ba87dfe4792cb20468a7a34a341b7f
Julio Santos <julio@morgane.com>
2009-10-16 19:27:55 -0700

    sum

--- inject.rb
+++ inject.rb
@@ -8,6 +8,9 @@ module Enumerable
   def my_reject(&block)
     self.inject([]) {|arr,i| if block.call(i) then arr else arr << i end}
   end
+  def my_sum
+    self.inject(0) {|total,i| total + i}
+  end
 end
 
 # select
@@ -24,3 +27,7 @@ p (1..10).my_map {|i| i % 2 == 0}
 p (1..10).reject {|i| i % 2 == 0}
 p (1..10).my_reject {|i| i % 2 == 0}
 # => [1, 3, 5, 7, 9]
+
+# sum/accu
+p (1..10).my_sum
+# => 55
