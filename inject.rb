7c989c885feb9aeb74151bb0aa2e189625de23e0
Julio Santos <julio@morgane.com>
2009-10-16 19:17:32 -0700

    inject methods

---
+++ inject.rb
@@ -0,0 +1,26 @@
+module Enumerable
+  def my_map(&block)
+    self.inject([]) {|arr,i| arr << block.call(i)}
+  end
+  def my_select(&block)
+    self.inject([]) {|arr,i| if block.call(i) then arr << i else arr end}
+  end
+  def my_reject(&block)
+    self.inject([]) {|arr,i| if block.call(i) then arr else arr << i end}
+  end
+end
+
+# select
+p (1..10).select {|i| i % 2 == 0}
+p (1..10).my_select {|i| i % 2 == 0}
+# => [2, 4, 6, 8, 10]
+
+# collect/map
+p (1..10).map    {|i| i % 2 == 0}
+p (1..10).my_map {|i| i % 2 == 0}
+# => [false, true, false, true, false, true, false, true, false, true]
+
+# reject
+p (1..10).reject {|i| i % 2 == 0}
+p (1..10).my_reject {|i| i % 2 == 0}
+# => [1, 3, 5, 7, 9]
