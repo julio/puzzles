4f78f4a445041dda5ee8e179d9b0d27467c446a4
Julio Santos <jsantos@Julios-Mac.local>
2010-01-25 11:49:54 -0800

    clean up

--- add.rb
+++ add.rb
@@ -2,8 +2,6 @@
 # Add large integers
 # ---------------------------------------------------------------------------
 
-require "test/unit"
-
 def prefix_array(arr, count, digit)
   0.upto(count-1) { arr.unshift(0) }
   arr
@@ -22,14 +20,19 @@ def add(n, m)
   total.unshift(carry).to_s.to_i
 end
 
+# =================== tests =================================================
+
+require "test/unit"
 class AddTest < Test::Unit::TestCase
   def test_add
-    assert_equal 5, add([2], [3])
-    assert_equal 444, add([1,2,3], [3,2,1])
-    assert_equal 4444, add([1,2,3], [4,3,2,1])
-    assert_equal 1000, add([9,9,9], [1])
+    assert_equal 0,      add([0,0,0,0,0,0], [0,0])
+    assert_equal 5,      add([2], [3])
+    assert_equal 444,    add([1,2,3], [3,2,1])
+    assert_equal 4444,   add([1,2,3], [4,3,2,1])
+    assert_equal 1000,   add([9,9,9], [1])
     assert_equal 199998, add([9,9,9,9,9], [9,9,9,9,9])
-    assert_equal 99999, add([0], [9,9,9,9,9])
-    assert_equal 1_000_000_000_000_000_000_000, add([1], [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9])
+    assert_equal 99999,  add([0], [9,9,9,9,9])
+    assert_equal 1_000_000_000_000_000_000_000, 
+                    add([1], [9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9])
   end
 end
