f3facbaa549c3661fc86bef8770545ae736d348f
Julio Santos <jsantos@Julios-Mac.local>
2010-01-25 11:02:46 -0800

    no need anymore

--- facebook/puzzle07/hoppity.rb
+++
@@ -1,9 +0,0 @@
-File.open(ARGV[0]) do |file|
-  1.upto(file.gets.chomp.to_i) do |i|
-    case 0
-      when i % 5 + i % 3 then puts "Hop"
-      when i % 3         then puts "Hoppity"
-      when i % 5         then puts "Hophop"
-    end
-  end
-end
