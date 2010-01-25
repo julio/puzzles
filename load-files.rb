1da5e1fe2ec411477d614cc0e7203b4ef9175eb6
Julio Santos <jsantos@Julios-Mac.local>
2010-01-25 11:40:24 -0800

    adding comments and cleaning up

--- load-files.rb
+++
@@ -1,9 +0,0 @@
-require 'open-uri'
-files = ""
-0.upto(27) do |index|
-  bits = open("http://www.cs.berkeley.edu/~bh/pdf/ssch%02d" % index, 'User-Agent' => 'Ruby-Wget').read
-  File.open("book/ss#{index}.pdf", 'w') {|f| f.write(bits)}
-  files += " book/ss#{index}.pdf"
-end
-
-system("gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=merged.pdf #{files}")
--- outer_join.sql
+++ outer_join.sql
@@ -1,3 +1,6 @@
+-- --------------------------------------------------------------------------
+-- Join
+-- --------------------------------------------------------------------------
 SELECT puzzles.name, count(solutions.puzzle_id) 
   FROM puzzles
   LEFT OUTER JOIN solutions ON puzzles.id = solutions.puzzle_id
