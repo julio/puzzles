eed14998c1890fd0b21077bc4760602e163f894d
Julio Santos <julio@morgane.com>
2012-05-25 07:35:42 -0700

    Refactor and comments

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,9 @@
+# Recursion without caching. Complexity is O(2^n)
 def fib1(n)
-  return 1 if n <= 1
-
-  fib1(n-1) + fib1(n-2)
+  n <= 1 ? 1 : fib1(n-1) + fib1(n-2)
 end
 
+# Recursion with caching. Complexity is O(n)
 def fib2(n)
   return 1 if n <= 1
 
@@ -13,17 +13,28 @@ def fib2(n)
   @store[n-1] + @store[n-2]
 end
 
+# Iterative. Complexity is O(n)
 def fib3(n)
   return 1 if n <= 1
 
   pair = [1,1]
   2.upto(n) do |x|
-    pair[0] = pair[0] + pair[1] if x%2 == 0
-    pair[1] = pair[0] + pair[1] if x%2 == 0
+    if x%2 == 0
+      pair[0] = pair[0] + pair[1]
+      pair[1] = pair[0] + pair[1]
+    end
   end
 
   n%2 == 0 ? pair[0] : pair[1]
 end
+
+p fib1(7)
+p fib2(7)
+p fib3(7)
+
+p fib1(8)
+p fib2(8)
+p fib3(8)
 
 require "benchmark"
 
